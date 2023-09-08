dedac055e26c6065fbe9a2ffe4da3181608ada35
alixvox <alixvox@gmail.com>
2023-09-08 11:17:40 -0500

    ,changing debounce delay to 500

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
   const button = $('<button>Click here to get started</button>');
-  button.on('click', _.debounce(updateCounter, 300));
+  button.on('click', _.debounce(updateCounter, 500));
   $('body').append(button);
   $('body').append('<p id="count"></p>');
   $('body').append('<p>Copyright - Holberton School</p>');
